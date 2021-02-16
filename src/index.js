1ab0f5d8c582a37c597ae61ba6c69438afc4c6a4
Adriana Venegas <adrivegas08@gmail.com>
2021-02-16 15:26:24 -0500

    Creación de la pantalla de registro

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { LoginScreen } from './components/auth/LoginScreen';
+// import { LoginScreen } from './components/auth/LoginScreen';
+import { RegisterScreen } from './components/auth/RegisterScreen';
 import './styles/styles.scss';
 
 ReactDOM.render(  
-  <LoginScreen />,  
+  <RegisterScreen />,  
   document.getElementById('root')
 );
 
