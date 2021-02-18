548359e87fccc704d2b424cbdc1a87e4a98761c3
Adriana Venegas <adrivegas08@gmail.com>
2021-02-18 18:10:37 -0500

    Configuración de archivo para rutas privadas

--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -1,18 +1,23 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import {
     BrowserRouter as Router,
     Switch,
     Route,
     Redirect
   } from 'react-router-dom';
+import { AuthContext } from '../auth/AuthContext';
 import { ConvertScreen } from '../components/convert/ConvertScreen';
 import { Planes } from '../components/convert/Planes';
 import { Tutoriales } from '../components/convert/Tutoriales';
 import { Footer } from '../components/Footer';
 import { Header } from '../components/Header';
 import { AuthRouter } from './AuthRouter';
+import { PrivateRoute } from './PrivateRoute';
 
 export const AppRouter = () => {
+
+    const { user } = useContext(AuthContext);
+    
     return (
         <Router>
             <div> 
@@ -41,10 +46,11 @@ export const AppRouter = () => {
                             component={ Tutoriales }
                         />
 
-                        <Route 
+                        <PrivateRoute 
                             exact
                             path="/planes"
                             component={ Planes }
+                            isAuthenticated={ user.logged }
                         />
 
                         <Redirect to='/auth/login' />  
