70aebd4bce585e18f1def081b017474d6a0bfc08
Adriana Venegas <adrivegas08@gmail.com>
2021-02-19 19:16:15 -0500

    Actualizacion de formulario de registro

--- src/components/auth/RegisterScreen.js
+++ src/components/auth/RegisterScreen.js
@@ -1,26 +1,78 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { useForm } from '../../hooks/useForm';
 
 export const RegisterScreen = () => {
+
+    const [ formValues, handleInputChange ] = useForm({
+        name: 'Adriana',
+        email: 'adri@gmail.com',
+        password: '123456',
+        password2: '123456',
+    });
+
+    const { name ,email ,password ,password2 } = formValues;
+
+    const handleRegister = (e) => {
+        e.preventDefault();
+
+        // if ( isFormValid() ) {
+        //     dispatch( startRegisterWithEmailPasswordName(email, password, name) );
+        // }
+
+    }
+
     return (
         <section className="container">           
 
             <div className="row d-flex justify-content-center m-5">
                 <div  className="col-5 card border-info ">
-                    <form className="card-body">
+                    <form className="card-body" onSubmit={ handleRegister }>
                         <fieldset className="">
                             <legend className="text-center">Regístrate</legend>                    
-                            <div class="form-group">
+                            <div className="form-group">
                                 <label for="exampleInputEmail1">Nombre</label>
-                                <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Nombre"/>
+                                <input                                     
+                                    className="form-control"                                     
+                                    type="text"
+                                    placeholder="Nombre"
+                                    name="name"
+                                    autoComplete="off"
+                                    value={ name }
+                                    onChange={ handleInputChange }
+                                />
                             </div>
                             <div class="form-group">
                                 <label for="exampleInputEmail1">Correo</label>
-                                <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="correo@mail.com"/>
+                                <input
+                                    type="email" 
+                                    class="form-control" 
+                                    placeholder="correo@mail.com"
+                                    value={ email }
+                                    onChange={ handleInputChange }
+                                />
                             </div>
                             <div class="form-group">
                                 <label for="exampleInputPassword1">Contraseña</label>
-                                <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Contraseña"/>
+                                <input 
+                                    type="password" 
+                                    class="form-control" 
+                                    placeholder="Contraseña"
+                                    name="password"
+                                    value={ password }
+                                    onChange={ handleInputChange }
+                                />
+                            </div> 
+                            <div class="form-group">
+                                <label for="exampleInputPassword1">Confirmar contraseña</label>
+                                <input 
+                                    type="password" 
+                                    class="form-control" 
+                                    placeholder="Confirme su contraseña"
+                                    name="password2"
+                                    value={ password2 }
+                                    onChange={ handleInputChange }
+                                />
                             </div> 
                             <div className="text-center">
                                 <button className=" text-center btn btn-primary btn-lg">Registrarme</button>
