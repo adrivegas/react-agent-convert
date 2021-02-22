967b55a463417791523bd593e2765bdf4e11d89b
Adriana Venegas <adrivegas08@gmail.com>
2021-02-21 20:40:18 -0500

    Muestra loading global de la aplicacion

--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -1,16 +1,19 @@
-import React, { useContext } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
+import { useDispatch } from 'react-redux';
 import {
     BrowserRouter as Router,
     Switch,
     Route,
     Redirect
-  } from 'react-router-dom';
-import { AuthContext } from '../auth/AuthContext';
+} from 'react-router-dom';
+import { login } from '../actions/auth';
+// import { AuthContext } from '../auth/AuthContext';
 import { ConvertScreen } from '../components/convert/ConvertScreen';
 import { Planes } from '../components/convert/Planes';
 import { Tutoriales } from '../components/convert/Tutoriales';
 import { Footer } from '../components/Footer';
 import { Header } from '../components/Header';
+import { firebase } from '../firebase/firebase-config';
 import { AuthRouter } from './AuthRouter';
 // import { ConvertRoutes } from './ConvertRoutes';
 // import { PrivateRoute } from './PrivateRoute';
@@ -19,6 +22,33 @@ import { AuthRouter } from './AuthRouter';
 export const AppRouter = () => {
 
     // const { user } = useContext(AuthContext);
+    const dispatch = useDispatch();
+
+    const [ checking, setChecking ] = useState(true);
+    const [ isLoggedIn, setIsLoggedIn ] = useState(false);
+
+    useEffect(() => {
+        
+        firebase.auth().onAuthStateChanged( (user) => {
+
+            if ( user?.uid ) {
+                dispatch( login( user.uid, user.displayName ) );
+                setIsLoggedIn( true );
+            } else {
+                setIsLoggedIn( false );
+            }
+
+            setChecking(false);
+
+        });
+        
+    }, [ dispatch, setChecking, setIsLoggedIn ])
+
+    if ( checking ) {
+        return (
+            <h1>Espere...</h1>
+        )
+    }
     
     return (
         <Router>
