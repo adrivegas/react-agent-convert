12057fc1df374236eda64f0c34a23032e7d74fb6
Adriana Venegas <adrivegas08@gmail.com>
2021-02-25 16:29:01 -0500

    download added

--- src/components/convert/Tutoriales.js
+++ src/components/convert/Tutoriales.js
@@ -5,12 +5,12 @@ export const Tutoriales = () => {
     
     return (
         
-        <section className="container mt-5 pb-5">
+        <section className="container margin mt-5 pb-5">
             
             <h1 className="display-5 text-center mb-3">Instrucciones de Uso</h1>   
             <UploadFile />
 
-            <div className="row row-cols-2 mb-5">
+            {/* <div className="row row-cols-2 mb-5">
                 <div className="col text-right"><h1>1</h1></div>
                 <div className="col text-left">
                     <p>Prepara el formato tecnico .CSV de tu agente. <small>Puedes descargar una plantilla de ejemplo</small></p>
@@ -50,7 +50,7 @@ export const Tutoriales = () => {
                     </button>
                 </div>
             
-            </div>
+            </div> */}
             
 
             
--- src/components/convert/UploadFile.js
+++ src/components/convert/UploadFile.js
@@ -9,8 +9,6 @@ export const UploadFile = () => {
         error: null,
     })
 
-    // state = { selectedFile: null }
-
     const fileChangedHandler = event => {
         setStateFile({ selectedFile: event.target.files[0] })
     }
@@ -50,62 +48,123 @@ export const UploadFile = () => {
             )
     }
 
-    return (
-        <div>
-            <h1>Upload File</h1>
+    const convertHandler = () => {
+        console.log('convert');
+        axios.get('https://suis1hxnd9.execute-api.us-east-1.amazonaws.com/dev/convert');       
+    }
 
-            <div>               
+    const downloadHandler = () => {
+        
+        console.log('xxxx');
+        axios.get('https://suis1hxnd9.execute-api.us-east-1.amazonaws.com/dev/download')
+
+        .then(response=>{
+            const url = response.data
+            console.log(response.data);          
+            let elm = document.createElement('a');
+            elm.href = url;
+            elm.click();
+        })
+        
+
+    }
+
+    return (
+        <div>           
+            <div className="mb-5">  
+                <h3> <span className="badge badge-pill badge-primary">1</span> Carga de archivos</h3>
+                <p className="lead">Para iniciar el proceso de conversión carga los archivos de tu agente en formato .csv</p>             
                 <form>
-                        <div className="form-group">
-                            <input
-                                type="file"
-                                className="form-control-file"
-                                id="fileUpload"
-                                onChange={fileChangedHandler}
-                            />
-                            <button
-                                type="button"
-                                className="btn btn-light"
-                                onClick={uploadHandler}
-                            >
-                                Upload your file
-                            </button>
+                    <div className="row form-group">
+                        <div className="col text-center">
+                            <div className="border border-primary rounded p-3">
+                                <input
+                                    type="file"
+                                    className="form-control-file"
+                                    id="fileUpload"
+                                    onChange={fileChangedHandler}
+                                />
+
+                                <button
+                                    type="button"
+                                    className="btn btn-primary"
+                                    onClick={uploadHandler}
+                                >
+                                    Cargar archivo 1
+                                </button>
+
+                                <div>
+                                    <span>
+                                        {stateFile.uploadSuccess
+                                            ? "Archivo cargado con éxito!"
+                                            : ""}
+                                    </span>
+                                </div>
+                            </div>                           
+
+                        </div>
+                        <div  className="col text-center ">
+                            <div className="border border-primary rounded p-3 ">
+                                <input
+                                    type="file"
+                                    className="form-control-file"
+                                    id="fileUpload"
+                                    onChange={fileChangedHandler}
+                                />
+
+                                <button
+                                    type="button"
+                                    className="btn btn-primary"
+                                    onClick={uploadHandler}
+                                >
+                                    Cargar archivo 2
+                                </button>
+
+                                <div>
+                                    <span>
+                                        {stateFile.uploadSuccess
+                                            ? "Archivo cargado con éxito!"
+                                            : ""}
+                                    </span>
+                                </div>
+                            </div>
                             
 
-                            <div>
-                                <span>
-                                    {stateFile.uploadSuccess
-                                        ? "File Upload Successfully"
-                                        : ""}
-                                </span>
-                            </div>
                         </div>
+                    </div>
                 </form>
             </div>
 
-            <div>
+            <div className="mb-5">
+                <h3> <span className="badge badge-pill badge-primary">2</span> Conversión de archivos</h3>
+                <p className="lead">Inicia la conversión automática de tus archivos....</p>
+                <div>
                     <div>
                         <button
-                            // onClick={e => {
-                            //     this.convert();
-                            // }}
-                        
+                            className= "btn btn-primary"
+                            onClick={ convertHandler }
                         >
-                            convertir
+                            Convertir
                         </button>
-                    </div>
+                    </div>                    
+                </div>                    
+            </div>
+
+            <div>
+                <h3> <span className="badge badge-pill badge-primary">3</span> Descarga archivo .zip</h3>
+                <p className="lead">Descarga tu agente Dialogflow y....</p>
+                <div>
                     <div>
-                        <button id="xxx" 
-                            // onClick={e => {
-                            //     this.download();
-                            // }}
+                        <button
+                            className= "btn btn-primary"
+                            onClick={ downloadHandler }
                         >
                             Descargar
                         </button>
                     </div>
                 </div>
-
-           
+                    
+            </div>
         </div>
     )
 }
